a0caa5f863e1cf1b97ffd0e061983ff3981e7adc
r3h6 <r3h6@outlook.com>
2016-05-27 23:06:00 +0200

    [CLEANUP] PhpDoc

--- Classes/Configuration/ExtensionConfiguration.php
+++ Classes/Configuration/ExtensionConfiguration.php
@@ -2,8 +2,26 @@
 
 namespace R3H6\Error404page\Configuration;
 
+/*                                                                        *
+ * This script is part of the TYPO3 project - inspiring people to share!  *
+ *                                                                        *
+ * TYPO3 is free software; you can redistribute it and/or modify it under *
+ * the terms of the GNU General Public License version 3 as published by  *
+ * the Free Software Foundation.                                          *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General      *
+ * Public License for more details.                                       *
+ *                                                                        */
+
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
+/**
+ * ExtensionConfiguration
+ *
+ * API to access extension configuration (ext_conf_template.txt).
+ */
 class ExtensionConfiguration implements \TYPO3\CMS\Core\SingletonInterface
 {
 
