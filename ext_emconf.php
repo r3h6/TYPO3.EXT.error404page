2a73059609005f8ca3416da8765598ffc4a4200f
Remo Häusler <r.haeusler@frappant.ch>
2016-11-21 17:54:13 +0100

    [TASK] Set version number to 2.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,24 +11,24 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Error 404 page',
-	'description' => 'Use TYPO3 pages for display 404 errors. Optional error 403 redirect handling. Works for multi domain and multilingual installations.',
-	'category' => 'fe',
-	'author' => 'R3 H6',
-	'author_email' => 'r3h6@outlook.com',
-	'state' => 'beta',
-	'internal' => '',
-	'uploadfolder' => '0',
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'version' => '2.0.0',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.0-7.6.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
+    'title' => 'Error 404 page',
+    'description' => 'Use TYPO3 pages for display 404 errors. Optional error 403 redirect handling. Works for multi domain and multilingual installations.',
+    'category' => 'fe',
+    'author' => 'R3 H6',
+    'author_email' => 'r3h6@outlook.com',
+    'state' => 'beta',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '2.1.0',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '6.2.0-7.6.99',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
 );
