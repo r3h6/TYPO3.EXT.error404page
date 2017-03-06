6a78f2a7bfd25fbb5a0f6b0f1af126d28eb506f3
r3h6 <r3h6@outlook.com>
2017-03-06 22:36:39 +0100

    [TASK] Last changes

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,10 @@ $EM_CONF[$_EXTKEY] = array(
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '2.1.1',
+    'version' => '3.0.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-7.6.99',
+            'typo3' => '7.6.0-8.99.99',
         ),
         'conflicts' => array(
         ),
